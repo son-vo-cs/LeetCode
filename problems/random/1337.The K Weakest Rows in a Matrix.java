f69cd93a8ec7dbcc6ff3e5df47d68f638e731dda
son-vo-cs <sonvo.cs@gmail.com>
2020-05-07 02:24:42 -0700

    first

---
+++ problems/random/1337.The K Weakest Rows in a Matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public int[] countSoldiers(int[][] mat)
+    {
+        int[] result = new int[mat.length];
+        for (int i = 0; i < mat.length; i++)
+        {
+            for (int j = 0; j < )
+        }
+    }
+    
+    
+    public int[] kWeakestRows(int[][] mat, int k) {
+        
+    }
+}
