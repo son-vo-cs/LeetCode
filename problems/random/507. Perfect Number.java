92e6430def3b0deec6f9dcd9e5aa06dd4de89fc9
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2020-09-21 23:18:07 -0700

    Update 507. Perfect Number.java

--- problems/random/507. Perfect Number.java
+++ problems/random/507. Perfect Number.java
@@ -12,7 +12,7 @@ Explanation: 28 = 1 + 2 + 4 + 7 + 14
 class Solution {
     public boolean checkPerfectNumber(int num) {
         if (num <= 1)
-            return false;
+            return false; 
          int sum = 1;
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
