d17d380120dcb830b48cb4c043b1b9a2e11e3d28
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2020-12-03 23:48:57 -0800

    Update 507. Perfect Number.java

--- problems/random/507. Perfect Number.java
+++ problems/random/507. Perfect Number.java
@@ -15,7 +15,7 @@ class Solution {
             return false; 
          int sum = 1;
         for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
+            if (num % i == 0) { 
                 sum += i;
                 if (i * i != num) {
                     sum += num / i;
