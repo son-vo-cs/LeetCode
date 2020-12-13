bcc38422d70df8b8f93532475642f8881d399d51
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2020-12-13 02:51:37 -0800

    Create 476. Math Flip bit Number Complement.java

---
+++ problems/Important/476. Math Flip bit Number Complement.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/number-complement/
+class Solution {
+  public int findComplement(int num) {
+    int todo = num, bit = 1;
+    while (todo != 0) {
+      // flip current bit
+      num = num ^ bit;
+      // prepare for the next run
+      bit = bit << 1;
+      todo = todo >> 1;
+    }
+    return num;
+  }
+}
