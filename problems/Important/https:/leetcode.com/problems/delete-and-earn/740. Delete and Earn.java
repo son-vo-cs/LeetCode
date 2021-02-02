29539c568f011be3ed447f806c9bed310eb0261e
son-vo-cs <43371971+son-vo-cs@users.noreply.github.com>
2021-02-02 10:04:39 -0800

    Create 740. Delete and Earn.java

---
+++ problems/Important/https:/leetcode.com/problems/delete-and-earn/740. Delete and Earn.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/delete-and-earn/
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int[] mem = new int[10001];
+        int max = 0;
+        for (int n : nums)
+        {
+            mem[n]++;
+            max = Math.max(max, n);
+        }
+        int[] dp = new int[10001];
+        dp[1] = mem[1];
+        for (int i = 2; i <= max; i++)
+        {
+            dp[i] = Math.max(dp[i-2] + mem[i]*i, dp[i-1]);
+            
+        }
+        return dp[max];
+    }
+}
